0cfc4365db60189efcf14be419238681838d7dc4
Abbos <abbosr180@gmail.com>
2022-11-11 19:27:22 +0500

    I have written my first programming

--- #02_Python_Darslari_Birinchi_Dasturimiz.py
+++ #02_Python_Darslari_Birinchi_Dasturimiz.py
@@ -1,5 +1,20 @@
 print('Assalom alaykum')
 print('Hayrli tong!')
-print(2+4*2)
-print(19/5)
-print(2**4)
+print(2 + 4 * 2)
+print(19 / 5)
+print(2 ** 4)
+#Bu yerda comment bor
+"""Bu yerda ham comment bor"""
+'''Bu yerda ham comment bor'''
+print("""
+Hello
+ World
+ Salom
+ Dunyo""")
+
+print('''
+Hello
+ World
+ Salom
+ Dunyo''')
+
