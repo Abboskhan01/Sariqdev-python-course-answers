aed0808ad2269eb46f83883990afaf642e4dbacb
Abbos <abbosr180@gmail.com>
2022-11-12 10:34:21 +0500

    I have finished homework of lesson 3

---
+++ #03Python_Darslari_print_Arifmetik_amallar_va_Sinteks.py
@@ -0,0 +1,20 @@
+# AMALIYOT
+# Quyidagi matnni aynan shunday ko'rinishda konsolda chiqaring:
+# "Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+# Quyidagi misollarga yechimni Pythonda chiqaring. Har bir misoldan avval misol matnini izoh ko'rinishida yozing:
+# 5 ning 4-darajasini toping
+# 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+# Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+# Diametri 12 ga teng bo'lgan doiraning yuzini toping  ( deb oling)
+# Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+# Javoblarni savol va yechim ko'rinishida chiqaring: 5 ning 4-darajasi 625
+
+# Answers
+
+print("\"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar havas")
+print('5 ning 4-darajasi', 5 ** 4, "ga teng")
+print("Tomonlarin 125ga teng bo'lgan kvadratning yuzi", 125**2, "ga teng")
+print("Diametri 12 ga teng bo'lgan doiraning yuzi", 3.14*12*2, 'ga teng')
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", 100, 'ga teng')
+"""I have finished lesson3 homework"""
+
