ec9d2b238e5af4c59faed98e735e523a0b4ac8c6
Abbos <abbosr180@gmail.com>
2022-11-12 10:51:17 +0500

    I did lesson4 homework

---
+++ 04_O'ZGARUVCHILAR.py
@@ -0,0 +1,33 @@
+ism = "Abdulloh"
+yosh = 25
+print(ism)
+print(yosh)
+ism = "Abdulloh"
+print(ism)
+ism = "Muhammad"
+print(ism)
+
+# AMALIYOT
+# Quyidagi mashqlarni bajaring:
+# "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+# xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+# class den nomlangan o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?)
+# Quyidagi kodni bajaring:
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+# Answers
+a = 'Hello World'
+print(a)
+xabar = 'Bugun havo juday zo\'r'
+print(xabar)
+xabar = 'Afsus yomg\'ir yog\'adi shekili'
+print(xabar)
+# class = 'Book'
+# print(class)
+"""It does not work  so I put it in the comments"""
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi =", aylana_yuzi, 'ga teng')
