d4d17476133f55fe1c7463efe48a7f9747e5e1b2
Abbos <abbosr180@gmail.com>
2022-12-16 18:46:59 +0500

    added new file

--- lugat-elementlari-bilan-ishlash.py
+++ lugat-elementlari-bilan-ishlash.py
@@ -41,3 +41,41 @@
 # for buyum in bozorlik:
 #     if buyum not in mahsulotlar:
 #         print(f"Iltimos, do'koningizga {buyum} ham olib keling")
+
+
+# Homework
+# Python izohli lug'atini yarating va lug'atga kamida 10 ta so'z qo'shing.
+# Lug'atdagi har bir kalit va qiymatni for tsikli yordamida, alifbo ketma-ketligida chiroyli qilib konsolga chiqaring.
+
+# Davlatlar va ularning poytaxtlari lug'atini tuzing. Avval lug' \
+# 'atdagi davlatlarni, keyin poytaxtlarni alohida-alohida, alifbo ketma-ketligida konsolga chiqaring.
+
+#
+# Foydalanuvchidan istalgan davlatni kiritishni so'rang va shu davlatning poytaxtini konsolga chiqaring.' \
+#  Agar foydalanuvchi lug'atda yo'q davlatni kiritsa, "Bizda bunday ma'lumot yo'q" degan xabarni chiqaring
+#
+# Restoran menusi lug'atini tuzing (kamida 10 ta taom-narh juftligini kiriting).
+# Foydalanuvchidan 3 ta ovqat buyurtma berishni so'rang. Foydalanuvchi kiritgan taomlarni menu bilan solishtiring,
+# agar taom menuda bo'lsa narhini ko'rsating, aks holda "bizda bunday taom yo'q" degan xabarni chiqaring
+
+# Answers
+# python_lugat = {'dict':'dictionary', 'title':'bosh harfni katta qiladi', 'sorted':'sortlab beradi alifbo boyicha' }
+# for k, q in python_lugat.items():
+#     print(f"{k} - {q}")
+
+
+country_capital = {'USA' : 'Washington DC', 'Uzbekistan': 'Tashkent', 'Spain' : 'Madrid'}
+print('Dunyo davlatlari : ')
+for k in sorted(country_capital.keys()):
+    print(k)
+
+print('Dunyo davlatlari poytaxtlari : ')
+for q in sorted(country_capital.values()):
+    print(q)
+
+country_capital = country_capital.keys()
+countrys = []
+for k in country_capital:
+    countrys.append(k)
+country = input('Istalgan davlat nomini kiriting : ')
+country = country_capital.get(country.lower())
