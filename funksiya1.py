2b46f549f0ae27a74202850af39db80a52268a9f
Abbos <abbosr180@gmail.com>
2023-01-08 12:05:55 +0500

    it has been finished by Abbos

--- funksiya1.py
+++ funksiya1.py
@@ -10,44 +10,49 @@
 # Answers
 
 
-# def born(year=2022):
-#     name = input('enter your name here >>>')
-#     age = input('enter your age here >>>')
-#     wasborn = year - int(age)
-#     print(f"{name} you was born {wasborn}")
-#
-#
-# born()
+def born(year=2022):
+    name = input('enter your name here >>>')
+    age = input('enter your age here >>>')
+    wasborn = year - int(age)
+    print(f"{name} you was born {wasborn}")
 
-#
-# def calculate():
-#     number = float(input('enter your number here >>> '))
-#     print(f"{number} ning kvadrati {number ** 2} ga teng")
-#     print(f"{number} ning kubi {number ** 3} ga teng")
-#
-#
-# calculate()
-# #
-#
-# def toq_juft():
-#     number = float(input('enter your number here >>> '))
-#     if number//2 == 0:
-#         print(f"{number} just son")
-#     else:
-#         print(f"{number} toq son")
-#
-# toq_juft()
+
+born()
+
+
+def calculate():
+    number = float(input('enter your number here >>> '))
+    print(f"{number} ning kvadrati {number ** 2} ga teng")
+    print(f"{number} ning kubi {number ** 3} ga teng")
+
+
+calculate()
 #
 
-# def daraja():
-#     x = float(input('enter your x here >>> '))
-#     y = float(input('enter your y here >>> '))
-#     print(f"{x}ning {y}darajasi {x**y}ga teng")
-#
-# daraja()
+def toq_juft():
+    number = float(input('enter your number here >>> '))
+    if number//2 == 0:
+        print(f"{number} just son")
+    else:
+        print(f"{number} toq son")
+
+toq_juft()
+
+
+def daraja():
+    x = float(input('enter your x here >>> '))
+    y = float(input('enter your y here >>> '))
+    print(f"{x}ning {y}darajasi {x**y}ga teng")
+
+daraja()
 
 
 def a(x, y=2):
     print(f"{x}ning {y}darajasi {x**y}ga teng")
 
 a(2)
+
+def qoldiq(x):
+    for k in range(2,10):
+        print(f"{x}ni {k}ga bolganda qoldiq={x%k}")
+qoldiq(100)
